b38d11db66a3af3faf37cf8dba88bdfaa3ef0493
Dan Diemer <dan@headway.io>
2024-08-05 12:14:33 -0500

    Feat: Add Privacy Policy handler for Termly

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true
+    swcMinify: true,
+    rewrites: async () => {
+        return [
+            {
+                source: '/:slug-privacy-policy',
+                destination: '/html/:slug-privacy-policy.html',
+            },
+        ];
+    }
+
 };
 
 module.exports = nextConfig;
